9ba574170b975337ee2114e9878663e56b673426
Faboo29 <fabien.liegard@gmail.com>
2019-02-27 15:04:25 -0500

    Language star implemented

--- src/components/Main/Languages/Language.js
+++ src/components/Main/Languages/Language.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import LevelStar from './Star.svg';
+import { TweenLite, Elastic } from 'gsap';
+
 
 class Language extends Component {
+    constructor(props) {
+        super(props);
+        this.starContainer = [];
+        this.starTween = null;
+    }
+
+    componentDidMount() {
+        this.starTween = this.animateStars(this.starContainer);
+    }
+
+    animateStars(stars) {
+        stars.forEach((star, index) => {
+            TweenLite.to(star, 1, {
+                y: '0', opacity: 1, scale: 1, ease: Elastic.easeOut.config(1, 0.3), delay: (2 + index * 0.1)
+            })
+        });
+    }
 
     render() {
         let starCount = this.props.content.level;
@@ -13,7 +33,19 @@ class Language extends Component {
                         alt={this.props.content.code} /> 
                 </div>
                 <div className="language-item__level">
-                    {starCount}
+                    <div className="star-wrapper">
+                    {
+                        [...Array(starCount)].map((star, index) => {
+                            return (
+                                <img 
+                                    src={LevelStar} 
+                                    key={index}
+                                    ref={ img => this.starContainer.push(img) }
+                                    alt="star" />
+                            )
+                        })
+                    }
+                    </div>
                 </div>
                 <div className="language-item__content">
                     <h2>{this.props.content.code}</h2>
